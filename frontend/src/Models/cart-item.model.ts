7bb1b32f43d1aa0ac05798204175c80cb7d4f408
Hussein <husseinstohy@gmail.com>
2025-02-19 03:37:41 +0200

    Delete frontend/src directory

--- frontend/src/Models/cart-item.model.ts
+++
@@ -1,9 +0,0 @@
-export interface CartItem {
-    id: string; // أو استخدم productId حسب البيانات الفعلية
-    bookDetails?: {
-      title: string;
-      price: number;
-    };
-    quantity: number;
-  }
-  
